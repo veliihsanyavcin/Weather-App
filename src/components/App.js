5023f7ab8cbf282ab8b17c66b5fc744fe20b7aa7
veliihsanyavcin <1120606032Vi>
2021-01-14 01:48:11 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Day from "./Day";
+import Night from "./Night";
+import styled from "styled-components";
+
+
+
+const App = () => {
+  return (
+  <Container>
+ <Day />,
+ <Night />
+  </Container>
+
+   
+ 
+    
+  
+    
+  );
+};
+
+const Container = styled.div`
+
+`;
+const Wrapper = styled.div`
+ 
+`;
+
+export default App;
+
---
+++ src/components/Night.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from "styled-components";
+import bgImg2 from "../assets/bg2.png";
+import sun2 from "../assets/sun.png";
+
+const night = () => {
+    return (
+        <Container>
+        
+        <Wrapper>
+        
+        </Wrapper>
+      </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
+const Wrapper = styled.div`
+ 
+`;
+
+
+export default night;
