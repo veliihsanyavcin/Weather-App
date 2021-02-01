882c992b79dd33b8646c2e890f863ff19e6a34fe
veliihsanyavcin <1120606032Vi>
2021-02-01 16:50:26 +0300

    Date format edited.

--- src/components/CurrentDate/CurrentDate.js
+++ src/components/CurrentDate/CurrentDate.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 const CurrentDate = ({ className }) => {
   const date = new Date();
-  var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  var day = days[ date.getDay() ];
-  var result = day + ', ' + date.toLocaleDateString() + ' ' + '|' + ' ' + date.toLocaleTimeString();
+  const a = date.toLocaleDateString('en-US', {
+    weekday:'long',
+    day : 'numeric', 
+    month : 'short',
+    year : 'numeric',
+  }) +' | '+
+  date.toLocaleString("en-US", {
+    hour12: true,
+    hour: 'numeric',
+    minute: '2-digit'});
+    console.log(a);
+  // var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  // var months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+  // var day = days[ date.getDay() ];
+  // var mounts = months[date.getMonth() ];
+  // console.log(mounts);  
+  var result = a;
   return (
     <div className={className}>
       {result}
